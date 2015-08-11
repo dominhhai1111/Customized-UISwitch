04b8e55fa4af25713b28b5a45e28201065c5fa61
Do Minh Hai <dominhhai@MacBook-Air.local>
2015-08-11 14:38:22 +0700

    Initial Commit

---
+++ UISwitch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UISwitch
+//
+//  Created by Do Minh Hai on 8/11/15.
+//  Copyright (c) 2015 Do Minh Hai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UISwitch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UISwitch
+//
+//  Created by Do Minh Hai on 8/11/15.
+//  Copyright (c) 2015 Do Minh Hai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
